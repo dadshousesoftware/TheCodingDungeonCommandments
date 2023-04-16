fcebf79eea6833cd3188f5b389dc8f11aae28d5f
dadshousesoftware <130874628+dadshousesoftware@users.noreply.github.com>
2023-04-15 21:26:12 -0400

    Add files via upload

---
+++ FunkyDNameGen.py
@@ -0,0 +1,5 @@
+print("Funky Discord Name Generator")
+d_name = input("Your current discord name?: ")
+d_tag = input("What is your discord tag #?: ")
+d_funkyname = d_name[::2]
+print("Your funky chunky discord name is: "+d_funkyname+"#"+d_tag)
